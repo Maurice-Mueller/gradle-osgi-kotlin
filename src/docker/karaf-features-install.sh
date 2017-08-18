4df39de5c1b5d7dfeb7d2a0402b72073793b45e1
Moe Pad <moepad380@googlemail.com>
2017-08-18 11:38:52 +0200

    added docker files

---
+++ src/docker/karaf-features-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd /opt/karaf/bin
+
+CXF_VERSION="3.1.10"
+JACKSON_VERSION="2.8.5"
+
+./client feature:install webconsole
+./client feature:repo-add cxf $CXF_VERSION
+./client feature:install http cxf-jaxrs cxf
+./client feature:repo-add mvn:org.code-house.jackson/features/$JACKSON_VERSION/xml/features
+./client feature:install jackson-jaxrs-json-provider jetty
+#./client bundle:install -s wrap:mvn:org.jetbrains.kotlin/kotlin-osgi-bundle/1.1.4
+./client bundle:install -s mvn:org.jetbrains.kotlin/kotlin-osgi-bundle/1.1.4
